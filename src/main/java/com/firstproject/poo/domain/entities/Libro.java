34d15868222c570f38e6bba50eeaa716da17b72e
pfranciscorojas <franciscorojas92@gmail.com>
2024-03-05 18:32:13 -0500

    first commit

---
+++ src/main/java/com/firstproject/poo/domain/entities/Libro.java
@@ -0,0 +1,24 @@
+package com.firstproject.poo.domain.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Libros")
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_libro")
+    private Long idLibro;
+
+    private String titulo;
+    @Column(name = "anio_publicacion")
+    private Integer anioPublicacion;
+    private Double precio;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_autor", nullable = false)
+    private Autor autor;
+
+    // Getters y setters
+}
+
