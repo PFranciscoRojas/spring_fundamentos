9afc4447c2f8d4c6bd74520df127f7f22e5908fb
pfranciscorojas <franciscorojas92@gmail.com>
2024-03-06 17:57:57 -0500

    add methods save delete

--- src/main/java/com/firstproject/poo/domain/repositories/LibroCrudRepository.java
+++ src/main/java/com/firstproject/poo/domain/repositories/LibroCrudRepository.java
@@ -1,8 +1,15 @@
 package com.firstproject.poo.domain.repositories;
 
 import com.firstproject.poo.domain.entities.Libro;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LibroCrudRepository extends CrudRepository<Libro,Long> {
+    @Query(value = "select * from libros where anio_publicacion  = ?", nativeQuery = true)
+    List<Libro> obtenerLibros(int anioPublicacion);
+
+    List<Libro> findByAnioPublicacionOrderByTituloDesc(int anioPublicacion);
 
 }
--- src/main/java/com/firstproject/poo/infraestructure/LibroRepository.java
+++ src/main/java/com/firstproject/poo/infraestructure/LibroRepository.java
@@ -11,4 +11,16 @@ public class LibroRepository {
     public List<Libro> obtenerTodo(){
         return (List<Libro>) libroRepo.findAll();
     }
+    public Libro save(Libro libro){
+        return libroRepo.save(libro);
+    }
+
+    public void delete(Libro libro){
+        libroRepo.delete(libro);
+    }
+
+    public List<Libro> obtenerByAnio(int anio){
+        return libroRepo.findByAnioPublicacion(anio);
+    }
+
 }
